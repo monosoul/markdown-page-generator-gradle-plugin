a2ec1e884bca686bd865f3ba5a7e071a0adf35bf
monosoul <Kloz.Klaud@gmail.com>
2019-04-10 15:18:48 +0500

    add gradle publish-plugin

--- build.gradle.kts
+++ build.gradle.kts
@@ -3,6 +3,7 @@ version = "2.1.0"
 
 plugins {
     `kotlin-dsl`
+    id("com.gradle.plugin-publish") version "0.10.1"
 }
 
 repositories {
@@ -23,5 +24,24 @@ gradlePlugin {
             id = "com.github.monosoul.markdown.page.generator"
             implementationClass = "com.github.monosoul.markdown.page.generator.gradle.plugin.MdPageGeneratorPlugin"
         }
+    }
+}
+
+pluginBundle {
+    (plugins) {
+        "mdPageGeneratorPlugin" {
+            displayName = "Markdown to HTML Page Generator Gradle Plugin"
+            description = "This plugins wraps the maven markdown-page-generator-plugin by walokra so it can be used in Gradle."
+            tags = listOf("markdown", "html", "header", "footer", "walokra")
+            version = project.version as String
+
+            website = "https://github.com/monosoul/markdown-page-generator-gradle-plugin"
+            vcsUrl = "https://github.com/monosoul/markdown-page-generator-gradle-plugin"
+        }
+    }
+
+    mavenCoordinates {
+        artifactId = project.name
+        version = project.version as String
     }
 }
