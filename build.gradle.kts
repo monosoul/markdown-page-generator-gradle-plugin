41025f9505213d684008871ed8eaa654b71c5da0
monosoul <Kloz.Klaud@gmail.com>
2019-04-10 12:29:32 +0500

    More proper build.gradle configuration

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,27 +1,14 @@
-import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
-
 group = "com.github.monosoul"
 version = "0.0.1"
 
 plugins {
-    kotlin("jvm") version "1.3.21"
+    `kotlin-dsl`
 }
-dependencies {
-    implementation(kotlin("stdlib-jdk8"))
-    implementation(gradleApi())
-}
+
 repositories {
-    mavenCentral()
+    jcenter()
 }
-tasks {
-    getByName("compileKotlin", KotlinCompile::class) {
-        kotlinOptions {
-            jvmTarget = "1.8"
-        }
-    }
-    getByName("compileTestKotlin", KotlinCompile::class) {
-        kotlinOptions {
-            jvmTarget = "1.8"
-        }
-    }
+
+kotlinDslPluginOptions {
+    experimentalWarning.set(false)
 }
