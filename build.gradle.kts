c7a6dc9f4822b09fe8457ce1e7bdbcd7b498f255
monosoul <Kloz.Klaud@gmail.com>
2019-04-10 12:47:22 +0500

    Add gradle plugin metadata

--- build.gradle.kts
+++ build.gradle.kts
@@ -16,3 +16,12 @@ kotlinDslPluginOptions {
 dependencies {
     implementation("com.ruleoftech:markdown-page-generator-plugin:2.1.0")
 }
+
+gradlePlugin {
+    plugins {
+        create("mdPageGeneratorPlugin") {
+            id = "com.github.monosoul.markdown.page.generator"
+            implementationClass = "com.github.monosoul.markdown.page.generator.gradle.plugin.MdPageGeneratorPlugin"
+        }
+    }
+}
