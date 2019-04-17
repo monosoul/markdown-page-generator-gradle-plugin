f23db06ecfb2772a15955a4914a7dfce46b186f1
monosoul <Kloz.Klaud@gmail.com>
2019-04-17 17:45:59 +0500

    make the plugin compatible with gradle 4.9 and higher

--- src/main/kotlin/com/github/monosoul/markdown/page/generator/gradle/plugin/MdPageGeneratorPlugin.kt
+++ src/main/kotlin/com/github/monosoul/markdown/page/generator/gradle/plugin/MdPageGeneratorPlugin.kt
@@ -3,14 +3,10 @@ package com.github.monosoul.markdown.page.generator.gradle.plugin
 import org.gradle.api.Plugin
 import org.gradle.api.Project
 
-import org.gradle.kotlin.dsl.*
-
 class MdPageGeneratorPlugin : Plugin<Project> {
 
     override fun apply(project: Project): Unit = project.run {
 
-        tasks {
-            register("generateHtml", GenerateHtmlTask::class)
-        }
+        tasks.register("generateHtml", GenerateHtmlTask::class.java)
     }
 }
