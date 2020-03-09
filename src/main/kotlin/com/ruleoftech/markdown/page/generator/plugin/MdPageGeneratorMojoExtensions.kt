430a48c450bdd054980fd5e3c2b12a167eb9c0ca
monosoul <andrei.nevedomskii@n26.com>
2020-03-09 12:53:42 +0100

    Refactor to optimize memory usage

--- src/main/kotlin/com/ruleoftech/markdown/page/generator/plugin/MdPageGeneratorMojoExtensions.kt
+++ src/main/kotlin/com/ruleoftech/markdown/page/generator/plugin/MdPageGeneratorMojoExtensions.kt
@@ -1,22 +1,47 @@
 package com.ruleoftech.markdown.page.generator.plugin
 
-var MdPageGeneratorMojo.defaultTitle by reflection<String>()
-var MdPageGeneratorMojo.alwaysUseDefaultTitle by reflection<Boolean>()
-var MdPageGeneratorMojo.headerHtmlFile by reflection<String>()
-var MdPageGeneratorMojo.footerHtmlFile by reflection<String>()
-var MdPageGeneratorMojo.failIfFilesAreMissing by reflection<Boolean>()
-var MdPageGeneratorMojo.recursiveInput by reflection<Boolean>()
-var MdPageGeneratorMojo.transformRelativeMarkdownLinks by reflection<Boolean>()
-var MdPageGeneratorMojo.inputEncoding by reflection<String>()
-var MdPageGeneratorMojo.outputEncoding by reflection<String>()
-var MdPageGeneratorMojo.parsingTimeoutInMillis by reflection<Long>()
+import java.lang.reflect.Field
+import kotlin.reflect.KProperty
+
+var MdPageGeneratorMojo.defaultTitle: String by ReflectionAccessor
+var MdPageGeneratorMojo.alwaysUseDefaultTitle: Boolean by ReflectionAccessor
+var MdPageGeneratorMojo.headerHtmlFile: String by ReflectionAccessor
+var MdPageGeneratorMojo.footerHtmlFile: String by ReflectionAccessor
+var MdPageGeneratorMojo.failIfFilesAreMissing: Boolean by ReflectionAccessor
+var MdPageGeneratorMojo.recursiveInput: Boolean by ReflectionAccessor
+var MdPageGeneratorMojo.transformRelativeMarkdownLinks: Boolean by ReflectionAccessor
+var MdPageGeneratorMojo.inputEncoding: String by ReflectionAccessor
+var MdPageGeneratorMojo.outputEncoding: String by ReflectionAccessor
+var MdPageGeneratorMojo.parsingTimeoutInMillis: Long by ReflectionAccessor
 fun MdPageGeneratorMojo.setInputFileExtensions(inputFileExtensions: String) {
     val field = getAccessibleField("inputFileExtensions")
 
     field.set(this, inputFileExtensions)
 }
 
-var MdPageGeneratorMojo.applyFiltering by reflection<Boolean>()
-var MdPageGeneratorMojo.timestampFormat by reflection<String>()
-var MdPageGeneratorMojo.attributes by reflection<Array<String>>()
-var MdPageGeneratorMojo.pegdownExtensions by reflection<String>()
+var MdPageGeneratorMojo.applyFiltering: Boolean by ReflectionAccessor
+var MdPageGeneratorMojo.timestampFormat: String by ReflectionAccessor
+var MdPageGeneratorMojo.attributes: Array<String> by ReflectionAccessor
+var MdPageGeneratorMojo.pegdownExtensions: String by ReflectionAccessor
+
+private fun getAccessibleField(fieldName: String): Field {
+    val field = MdPageGeneratorMojo::class.java.getDeclaredField(fieldName)
+    field.isAccessible.takeIf { !it }?.let { field.isAccessible = true }
+
+    return field
+}
+
+private object ReflectionAccessor {
+    @Suppress("UNCHECKED_CAST")
+    operator fun <T> getValue(thisRef: MdPageGeneratorMojo, property: KProperty<*>): T {
+        val field = getAccessibleField(property.name)
+
+        return field.get(thisRef) as T
+    }
+
+    operator fun <T> setValue(thisRef: MdPageGeneratorMojo, property: KProperty<*>, value: T) {
+        val field = getAccessibleField(property.name)
+
+        field.set(thisRef, value)
+    }
+}
